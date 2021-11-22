62e790474cde5e6bf56ac2e8adbdc843c1ffc4c3
Gema Segarra <gema.segb@gmail.com>
2021-11-22 15:41:26 +0100

    Add @EnableEurekaClient, @EnableFeignClients & @EnableEurekaServer annotations

--- backend/edge-server/src/main/java/com/segarra/edgeserver/EdgeServerApplication.java
+++ backend/edge-server/src/main/java/com/segarra/edgeserver/EdgeServerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableEurekaClient
 public class EdgeServerApplication {
 
 	public static void main(String[] args) {

504eed4fca6fc58ba3c8c07b69151aa8dee60b36
Gema Segarra <gema.segb@gmail.com>
2021-11-22 12:33:21 +0100

    First commit

---
+++ backend/games-service/src/main/java/com/segarra/gamesservice/GamesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.segarra.gamesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesServiceApplication.class, args);
+	}
+
+}

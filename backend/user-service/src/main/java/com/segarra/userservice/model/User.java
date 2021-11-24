41117ce60ca9daafef35bcfcafc6438b346f5ad6
Gema Segarra <gema.segb@gmail.com>
2021-11-24 01:09:20 +0100

    Create user model

--- backend/user-service/src/main/java/com/segarra/userservice/model/User.java
+++ backend/user-service/src/main/java/com/segarra/userservice/model/User.java
@@ -1,4 +1,91 @@
 package com.segarra.userservice.model;
 
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String platform;
+    // private List<Long> games;
+    // private List<Long> friends;
+
+    public User() {
+    }
+
+    public User(String username, String email, String platform) {
+        this.username = username;
+        this.email = email;
+        this.platform = platform;
+    }
+
+ /*   public User(String username, String email, String platform, List<Long> games) {
+        this.username = username;
+        this.email = email;
+        this.platform = platform;
+        this.games = games;
+    }
+
+    public User(String username, String email, String platform, List<Long> games, List<Long> friends) {
+        this.username = username;
+        this.email = email;
+        this.platform = platform;
+        this.games = games;
+        this.friends = friends;
+    }*/
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+ /*   public List<Long> getGames() {
+        return games;
+    }
+
+    public void setGames(List<Long> games) {
+        this.games = games;
+    }
+
+    public List<Long> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(List<Long> friends) {
+        this.friends = friends;
+    }*/
 }

6540d6038eb1c145c946a6b2b93b026ee2038105
Gema Segarra <gema.segb@gmail.com>
2021-12-11 22:47:20 +0100

    create join match model

---
+++ frontend/src/app/components/model/JoinMatch.ts
@@ -0,0 +1,33 @@
+export class JoinMatch {
+    constructor(
+    private _game: number,
+    private _user: number,
+    private _numberOfPlayers: number,
+    private _comment: string
+    ){
+    }
+    public get numberOfPlayers(): number {
+        return this._numberOfPlayers;
+    }
+    public set numberOfPlayers(value: number) {
+        this._numberOfPlayers = value;
+    }
+    public get user(): number {
+        return this._user;
+    }
+    public set user(value: number) {
+        this._user = value;
+    }
+    public get game(): number {
+        return this._game;
+    }
+    public set game(value: number) {
+        this._game = value;
+    }
+    public get comment(): string {
+        return this._comment;
+    }
+    public set comment(value: string) {
+        this._comment = value;
+    }
+}

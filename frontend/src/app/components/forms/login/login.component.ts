c5da7948accfd85f80d727fc1e967d21a76ae7d5
Gema Segarra <gema.segb@gmail.com>
2021-12-11 22:48:12 +0100

    create login form

--- frontend/src/app/components/forms/login/login.component.ts
+++ frontend/src/app/components/forms/login/login.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { LoginService } from 'src/app/services/login.service';
 import { User } from '../../model/User';
-
-
+import { catchError } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -14,10 +16,13 @@ export class LoginComponent implements OnInit {
   registerForm: FormGroup;
   nameInput: FormControl;
   passwordInput: FormControl;
-  user!: User;
+  user!: User;    
+  errorMessage!: string;
+  errorStatus!: number;
 
 
-  constructor(private loginService: LoginService) {
+
+  constructor(private loginService: LoginService, private router: Router) {
 
     this.nameInput = new FormControl('', [ Validators.required]);
     this.passwordInput = new FormControl('', [Validators.required]);
@@ -38,15 +43,42 @@ export class LoginComponent implements OnInit {
   
     this.user = new User(this.nameInput.value, this.passwordInput.value)
     this.loginService.login(this.user)
-      .subscribe(data => {
-        console.log('los datos *** ' + data)
-      })      
+      .pipe(catchError((err: HttpErrorResponse) => {
+        this.errorMessage = err.error.message;
+        this.errorStatus = err.error.status;
+        this.errorAlert();
+        throw new Error("error");
+      }))
+      .subscribe(user => {
+        this.alertWithSuccess();
+        this.router.navigate(['/games']);
+      });
   }
 
   onSubmit(): void {
-    console.log('Creating user...');
-    console.log(this.registerForm.value)
+    this.login();
+  
   }
 
+
+  errorAlert()  
+  {  
+    Swal.fire({  
+      icon: 'error',  
+      title: 'Login failed',  
+      text: this.errorMessage,  
+    })  
+  }
+
+  alertWithSuccess(){  
+    Swal.fire({   
+    title:'Welcome, ' + this.user.name,       
+    icon: 'success',
+    showConfirmButton: false,    
+    timer: 1500 
+  })  
+  } 
 }
+
+
 

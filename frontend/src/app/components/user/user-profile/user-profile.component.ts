37c1415c52d6483b6079572f9db8400aad276946
Gema Segarra <gema.segb@gmail.com>
2021-12-13 19:44:26 +0100

    User profile changes

--- frontend/src/app/components/user/user-profile/user-profile.component.ts
+++ frontend/src/app/components/user/user-profile/user-profile.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import { MatchingService } from 'src/app/services/matching.service';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
+import { MatchingOutput } from '../../model/MatchingOutput';
+import { UserProfile } from '../../model/UserProfile';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +14,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  username: any;
+  userId!: any;
+  user!: UserProfile;
+  matchId!: any;
+  matchesCreated: Array<MatchingOutput> = [];
+  matchesJoined: Array<MatchingOutput> = [];
+  panelOpenState!: boolean;
+
+  constructor(private userService: UserService,
+    private matchService: MatchingService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.username = localStorage.getItem('user');
+    this.findUserId();
+    this.findMatchRequests();
+    this.findMatchRequestsJoined();
   }
 
+  findUserId(): void{
+    this.userService.findUserIdByName(this.username).subscribe(data => {
+      this.userId = data;
+      console.log(this.userId)
+      this.findUser();
+      console.log(this.user + "el user")
+  });
+  }
+
+  findUser(): void{
+    this.userService.getUser(this.userId).subscribe(data => {
+      this.user = data;
+      console.log(data + "hola");
+    })
+  }
+
+  findMatchRequests(): void{
+    this.matchService.getMatchByUser(this.user.username).subscribe(data=> {
+     this.matchesCreated = data;
+    })
+  }
+  findMatchRequestsJoined(): void{
+    this.matchService.getMatchByMatcher(this.user.username).subscribe(data=> {
+     this.matchesJoined = data;
+    })
+  }
+
+  deleteAlert(id: number): void{
+    Swal.fire({
+      title: 'Are you sure you want to delete this match request?',
+      showDenyButton: true,
+      confirmButtonText: 'Cancel',
+      denyButtonText: 'Yes! Delete it!',
+    }).then((result) => {
+      if (result.isDenied) {
+        Swal.fire('Match deleted!', '', 'success')
+        this.deleteMatch(id);
+        this.router.navigate(['/matchinglist']);
+        this.reloadPage();
+      }
+    })
+    }
+    
+    deleteMatch(id: number): void{
+      this.matchService.deleteMatch(id).subscribe(()=>{
+      console.log('match deleted')});
+      }
+
+      reloadPage() {
+        setTimeout(()=>{
+          window.location.reload();
+        }, 100);
+    }
 }

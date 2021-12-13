3da4cba1eafafcd8beaa0a29a83ebb073218c109
Gema Segarra <gema.segb@gmail.com>
2021-12-13 19:43:03 +0100

    Create friend model

---
+++ frontend/src/app/components/model/Friend.ts
@@ -0,0 +1,13 @@
+export class Friend{
+    constructor(
+        private _name: string,
+      )
+      {}
+    
+      public get name(): string {
+        return this._name;
+      }
+      public set name(value: string) {
+        this._name = value;
+      }
+}
